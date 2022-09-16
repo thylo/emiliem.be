b463c267f9897c197fb27635be4392a7e7a9287a
Julien Moreau <julien@thylo.be>
2022-09-16 17:59:04 +0200

    implement content

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,3 +1,10 @@
 <?php snippet('header') ?>
-<h1><?= $page->title() ?></h1>
+<?php foreach (page()->sections()->toBlocks() as $block): ?>
+  <div id="<?= $block->id() ?>" class="o-section block block-type-<?= $block->type() ?>">
+    <?php snippet('blocks/' . $block->type(), [
+      'block' => $block,
+      'theme' => 'dark'
+    ]) ?>
+  </div>
+<?php endforeach ?>
 <?php snippet('footer') ?>
--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-import './index.css'
+import './scss/emiliem.scss'
 
 console.log('Hello kirby!')
