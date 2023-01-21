3f3b4a357f3b37d628fca8f20f00f5eb11c1b97f
Julien Moreau <julien@thylo.be>
2023-01-21 16:18:23 +0100

    update workshops

--- site/snippets/blocks/workshops.php
+++ site/snippets/blocks/workshops.php
@@ -4,12 +4,12 @@
 <div class="c-workshops" style="--bg-image:url(<?= $block->background_image()->toFile()->url() ?>)">
   <div class="o-wrapper">
     <div class="c-workshops__content">
-      <h2 class="c-title-4xl"><?= $block->title() ?></h2>
-      <ul class="o-list-bare">
+      <h2 class="c-title-4xl u-margin-bottom"><?= $block->title() ?></h2>
+      <ul class="o-list-bare c-workshops__list">
         <?php foreach ($block->workshops()->toStructure() as $workshop): ?>
-          <li class="u-margin-bottom o-list-bare__item c-workshop">
-            <h4><?= $workshop->title() ?></h4>
-            <div><?= $workshop->description()->markdown() ?></div>
+          <li class="o-list-bare__item c-workshop c-workshops__item">
+            <h4 class="c-title-2xl"><?= $workshop->title() ?></h4>
+            <div class="u-wysiwig-none c-workshop__price"><?= $workshop->description()->markdown() ?></div>
           </li>
         <?php endforeach; ?>
       </ul>
